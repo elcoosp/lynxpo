c304e73a08f7a2aa3993204a76ce3bc41de0ece3
elcoosp <elcoosp@gmail.com>
2025-03-18 23:26:40 +0100

    feat(nmb): moved ktts plugin

---
+++ packages/tools/nmb/nmb-ktts-plugin/plugin/src/main/kotlin/lynxpo/ktts/annotations.kt
@@ -0,0 +1,13 @@
+package lynxpo.ktts.annotations
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.SOURCE)
+public annotation class Typed(val value: String = "")
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+public annotation class TsRetInto(val value: String)
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.SOURCE)
+annotation class LynxMethod
