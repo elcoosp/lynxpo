52be51034a98e435b84e38e2589dd0e9eca2971c
elcoosp <elcoosp@gmail.com>
2025-03-22 21:31:24 +0100

    fix(core-kt): pass colorResId to setColor

--- packages/core/core-kt/lib/src/main/kotlin/lynxpo/core/modules/statusbar/StatusBar.kt
+++ packages/core/core-kt/lib/src/main/kotlin/lynxpo/core/modules/statusbar/StatusBar.kt
@@ -1,21 +1,20 @@
 @file:Typed
+
 package lynxpo.core.modules.statusbar
 
 import android.content.Context
 import androidx.core.content.ContextCompat
 import com.lynx.jsbridge.LynxMethod
 import lynxpo.core.ActivityAwareLynxpoModule
-import lynxpo.core.R
 import lynxpo.ktts.annotations.Typed
 
 /** Status bar manipulation, use api compat https://github.com/IODevBlue/StatusBarColors
  *  Should migrate to use https://developer.android.com/reference/kotlin/androidx/compose/foundation/layout/WindowInsets */
 class StatusBarModule(private val context: Context) : ActivityAwareLynxpoModule(context) {
     @LynxMethod
-    fun setColor() {
+    fun setColor(colorResId: Int) {
         withActivity { activity ->
-            val blueColor = ContextCompat.getColor(context, R.color.teal_200)
-            StatusBarColors.setStatusBarColor(activity, blueColor)
+            StatusBarColors.setStatusBarColor(activity, ContextCompat.getColor(context, colorResId))
         }
     }
 
