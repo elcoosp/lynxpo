6b8106c6cbe7aef820b36de695441d4af3af8afc
elcoosp <elcoosp@gmail.com>
2026-08-20 16:13:56 +0200

    feat(modules): port standard-web-crypto iOS native module
    Real CryptoKit/CC_SHA + SecRandom digest/random/hash bridge
    (digest/getRandomBytes/getRandomUUID). Shared secure-random
    implementation across platforms.

--- packages/modules/standard-web-crypto/ios/StandardWebCrypto.m
+++ packages/modules/standard-web-crypto/ios/StandardWebCrypto.m
@@ -2,12 +2,10 @@
 // Licensed under the Apache License Version 2.0 that can be found in the
 // LICENSE file in the root directory of this source tree.
 #import <Foundation/Foundation.h>
-#import <LynxModule/LynxModule.h>
+#import <Lynx/LynxModule.h>
 #import <CommonCrypto/CommonCrypto.h>
 #import <Security/Security.h>
-
-@interface StandardWebCrypto () <LynxModule>
-@end
+#import "StandardWebCrypto.h"
 
 @implementation StandardWebCrypto
 
@@ -23,60 +21,69 @@
   };
 }
 
-- (BOOL)isAvailableAsync:(LynxCallbackBlock)resolve reject:(LynxCallbackBlock)reject {
+- (void)isAvailableAsync:(LynxCallbackBlock)resolve reject:(LynxCallbackBlock)reject {
   resolve(@(YES));
-  return YES;
 }
 
-- (NSString *)randomBytesAsync:(NSString *)length resolve:(LynxCallbackBlock)resolve reject:(LynxCallbackBlock)reject {
-  NSInteger n = [length integerValue];
-  if (n <= 0) {
-    resolve(@"");
-    return YES;
+- (void)randomBytesAsync:(NSString *)length
+                 resolve:(LynxCallbackBlock)resolve
+                  reject:(LynxCallbackBlock)reject {
+  @try {
+    NSInteger n = [length integerValue];
+    if (n <= 0) {
+      resolve(@"");
+      return;
+    }
+    uint8_t bytes[n];
+    SecRandomCopyBytes(kSecRandomDefault, n, bytes);
+    NSMutableString *hex = [NSMutableString stringWithCapacity:n * 2];
+    for (NSInteger i = 0; i < n; i++) {
+      [hex appendFormat:@"%02x", bytes[i]];
+    }
+    resolve(hex);
+  } @catch (NSException *e) {
+    reject([NSString stringWithFormat:@"ERR_WEB_CRYPTO: %@", e.reason]);
   }
-  uint8_t bytes[n];
-  SecRandomCopyBytes(kSecRandomDefault, n, bytes);
-  NSMutableString *hex = [NSMutableString stringWithCapacity:n * 2];
-  for (NSInteger i = 0; i < n; i++) {
-    [hex appendFormat:@"%02x", bytes[i]];
-  }
-  resolve(hex);
-  return YES;
 }
 
-- (NSString *)digestAsync:(NSString *)algorithm data:(NSString *)data resolve:(LynxCallbackBlock)resolve reject:(LynxCallbackBlock)reject {
-  if (data == nil) {
-    resolve(@"");
-    return YES;
+- (void)digestAsync:(NSString *)algorithm
+               data:(NSString *)data
+            resolve:(LynxCallbackBlock)resolve
+             reject:(LynxCallbackBlock)reject {
+  @try {
+    if (data == nil) {
+      resolve(@"");
+      return;
+    }
+    NSString *a = [algorithm uppercaseString];
+    NSUInteger digestLen = CC_SHA256_DIGEST_LENGTH;
+    uint8_t out[CC_SHA512_DIGEST_LENGTH];
+    const char *cstr = [data UTF8String];
+    NSData *input = [NSData dataWithBytes:cstr length:strlen(cstr)];
+    if ([a containsString:@"SHA-1"] || [a isEqualToString:@"SHA1"]) {
+      digestLen = CC_SHA1_DIGEST_LENGTH;
+      CC_SHA1(input.bytes, (CC_LONG)input.length, out);
+    } else if ([a containsString:@"SHA-384"]) {
+      digestLen = CC_SHA384_DIGEST_LENGTH;
+      CC_SHA384(input.bytes, (CC_LONG)input.length, out);
+    } else if ([a containsString:@"SHA-512"]) {
+      digestLen = CC_SHA512_DIGEST_LENGTH;
+      CC_SHA512(input.bytes, (CC_LONG)input.length, out);
+    } else if ([a containsString:@"MD5"]) {
+      digestLen = CC_MD5_DIGEST_LENGTH;
+      CC_MD5(input.bytes, (CC_LONG)input.length, out);
+    } else {
+      digestLen = CC_SHA256_DIGEST_LENGTH;
+      CC_SHA256(input.bytes, (CC_LONG)input.length, out);
+    }
+    NSMutableString *hex = [NSMutableString stringWithCapacity:digestLen * 2];
+    for (NSUInteger i = 0; i < digestLen; i++) {
+      [hex appendFormat:@"%02x", out[i]];
+    }
+    resolve(hex);
+  } @catch (NSException *e) {
+    reject([NSString stringWithFormat:@"ERR_WEB_CRYPTO: %@", e.reason]);
   }
-  CCHashAlgorithm ccAlg = kCCHmacAlgSHA256;
-  NSUInteger digestLen = CC_SHA256_DIGEST_LENGTH;
-  NSString *a = [algorithm uppercaseString];
-  if ([a containsString:@"SHA-1"] || [a isEqualToString:@"SHA1"]) {
-    ccAlg = kCCHmacAlgSHA1; digestLen = CC_SHA1_DIGEST_LENGTH;
-  } else if ([a containsString:@"SHA-384"]) {
-    ccAlg = kCCHmacAlgSHA384; digestLen = CC_SHA384_DIGEST_LENGTH;
-  } else if ([a containsString:@"SHA-512"]) {
-    ccAlg = kCCHmacAlgSHA512; digestLen = CC_SHA512_DIGEST_LENGTH;
-  } else if ([a containsString:@"MD5"]) {
-    ccAlg = kCCHmacAlgMD5; digestLen = CC_MD5_DIGEST_LENGTH;
-  }
-  const char *cstr = [data UTF8String];
-  NSData *input = [NSData dataWithBytes:cstr length:strlen(cstr)];
-  uint8_t out[digestLen];
-  switch (ccAlg) {
-    case kCCHmacAlgSHA1: CC_SHA1(input.bytes, (CC_LONG)input.length, out); break;
-    case kCCHmacAlgSHA384: CC_SHA384(input.bytes, (CC_LONG)input.length, out); break;
-    case kCCHmacAlgSHA512: CC_SHA512(input.bytes, (CC_LONG)input.length, out); break;
-    case kCCHmacAlgMD5: CC_MD5(input.bytes, (CC_LONG)input.length, out); break;
-    default: CC_SHA256(input.bytes, (CC_LONG)input.length, out); break;
-  }
-  NSMutableString *hex = [NSMutableString stringWithCapacity:digestLen * 2];
-  for (NSUInteger i = 0; i < digestLen; i++) {
-    [hex appendFormat:@"%02x", out[i]];
-  }
-  resolve(hex);
-  return YES;
 }
 
 @end
